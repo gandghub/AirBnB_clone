66f859b6caab6862861a6fec265e89dd1d049d8f
SUBANUM63 <zeinuka641@gmail.com>
2024-02-11 04:18:00 -0800

    7. Console 0.1

--- models/base_model.py
+++ models/base_model.py
@@ -13,7 +13,7 @@ Module for the Base Class
 
 import uuid
 from datetime import datetime
-from models import storage
+import models
 
 
 class BaseModel:
@@ -56,7 +56,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            storage.new(self)
+            models.storage.new(self)
 
     def __str__(self):
         """Returns a string representation of the object class"""
@@ -69,7 +69,7 @@ class BaseModel:
         with the current datetime
         """
         self.updated_at = datetime.now()
-        storage.save()
+        models.storage.save()
 
     def to_dict(self):
         """
